01471f8dcac3159c349ff1d24bf32527bceeb472
xiaoruib <xiaoruib@yonyou.com>
2023-05-05 15:42:03 +0800

    配置Mysql与注册登陆模块（上）

--- backend/src/test/java/com/kob/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/kob/backend/BackendApplicationTests.java
@@ -2,12 +2,20 @@ package com.kob.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+        System.out.println(bCryptPasswordEncoder.encode("pb"));
+        System.out.println(bCryptPasswordEncoder.encode("pc"));
+        System.out.println(bCryptPasswordEncoder.encode("pd"));
+        System.out.println(bCryptPasswordEncoder.encode("pe"));
+
     }
 
 }
