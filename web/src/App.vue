4c66874ffec1414468a52d99ce1797d3c81daad9
xiaoruib <xiaoruib@yonyou.com>
2023-05-06 17:18:01 +0800

    配置Mysql与注册登陆模块（中）

--- web/src/App.vue
+++ web/src/App.vue
@@ -8,10 +8,28 @@
 import 'bootstrap/dist/js/bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import NavBar from "@/components/NavBar.vue";
+// import $ from 'jquery';
 
 export default {
     components: {
         NavBar
+    },
+    setup() {
+        // $.ajax({
+        //     url: 'http://localhost:3000/user/account/register/',
+        //     type: 'post',
+        //     data: {
+        //         username: 'yxca',
+        //         password: '123',
+        //         confirmedPassword: '123',
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // });
     }
 }
 
