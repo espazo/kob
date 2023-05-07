0a15fd1cebdb2ca1ae58ac0d496c85244a533e6c
Julius <espazo@outlook.com>
2023-05-08 01:17:08 +0800

    5.1:30.10 创建个人中心页面（上）

--- web/src/views/pk/PkIndexView.vue
+++ web/src/views/pk/PkIndexView.vue
@@ -5,10 +5,34 @@
 <script>
 
 import PlayGround from "@/components/PlayGround.vue";
+import $ from "jquery";
+import {useStore} from "vuex";
 
 export default {
     name: "PkIndexView",
     components: {PlayGround},
+  setup() {
+    const store = useStore();
+
+    $.ajax({
+      url: "http://localhost:3000/user/bot/add/",
+      type: "post",
+      headers: {
+        Authorization: "Bearer " + store.state.user.token,
+      },
+      data: {
+        title: "title",
+        description: "description",
+        content: "content",
+      },
+      success(resp) {
+        console.log(resp);
+      },
+      error(resp) {
+        console.log(resp);
+      }
+    });
+  }
 }
 </script>
 
