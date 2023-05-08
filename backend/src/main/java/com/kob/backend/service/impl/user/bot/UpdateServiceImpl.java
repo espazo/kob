1bcb34dced6411a482be082f1d73ac1758885280
xiaoruib <xiaoruib@yonyou.com>
2023-05-08 19:37:18 +0800

    5.1, 52:35 创建个人中心页面（上）

---
+++ backend/src/main/java/com/kob/backend/service/impl/user/bot/UpdateServiceImpl.java
@@ -0,0 +1,52 @@
+package com.kob.backend.service.impl.user.bot;
+
+import com.kob.backend.mapper.BotMapper;
+import com.kob.backend.pojo.Bot;
+import com.kob.backend.pojo.User;
+import com.kob.backend.service.impl.utils.UserDetailsImpl;
+import com.kob.backend.service.user.bot.UpdateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class UpdateServiceImpl implements UpdateService {
+
+    @Autowired
+    BotMapper botMapper;
+
+    @Override
+    public Map<String, String> update(Map<String, String> data) {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+
+        UserDetailsImpl loginUser = (UserDetailsImpl) authenticationToken.getPrincipal();
+        User user = loginUser.getUser();
+
+        Integer bot_id = Integer.parseInt(data.get("bot_id"));
+
+        String title = data.get("title");
+        String description = data.get("description");
+        String content = data.get("content");
+
+        Map<String, String> map = new HashMap<>();
+
+        Bot bot = botMapper.selectById(bot_id);
+        if (!bot.getUserId().equals(user.getId())) {
+            map.put("error_message", "没有权限");
+            return map;
+        }
+
+        Bot new_bot = new Bot(bot.getId(), bot.getUserId(), title, description, content, bot.getRating(), bot.getCreatetime(), new Date());
+        botMapper.updateById(new_bot);
+
+        map.put("error_message", "scuess");
+
+        return map;
+    }
+}
--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -4,10 +4,71 @@
 
 <script>
 import ContentField from "@/components/ContentField.vue";
+import {useStore} from "vuex";
+import $ from "jquery";
 
 export default {
     name: "UserBotIndexView",
-    components: {ContentField}
+    components: {ContentField},
+    setup() {
+        const store = useStore();
+
+        // $.ajax({
+        //     url: "http://localhost:3000/user/bot/add/",
+        //     type: "post",
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     data: {
+        //         title: "title",
+        //         description: "description",
+        //         content: "content",
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // });
+
+        // $.ajax({
+        //     url: "http://localhost:3000/user/bot/remove/",
+        //     type: "post",
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     data: {
+        //         bot_id: 1,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     },
+        // });
+
+        $.ajax({
+            url: "http://localhost:3000/user/bot/update/",
+            type: "post",
+            headers: {
+                Authorization: "Bearer " + store.state.user.token,
+            },
+            data: {
+                bot_id: 1,
+                title: "title",
+                description: "description",
+                content: "content",
+            },
+            success(resp) {
+                console.log(resp);
+            },
+            error(resp) {
+                console.log(resp);
+            }
+        });
+    }
 }
 </script>
 
