30d053e2a69655424c9b39eaf2644516fe46c5cd
xiaoruib <xiaoruib@yonyou.com>
2023-06-01 09:29:58 +0800

    6.2 实现微服务：匹配系统（中）

--- backend/src/main/java/com/kob/backend/consumer/utils/Game.java
+++ backend/src/main/java/com/kob/backend/consumer/utils/Game.java
@@ -1,21 +1,70 @@
 package com.kob.backend.consumer.utils;
 
+import com.alibaba.fastjson2.JSONObject;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.kob.backend.consumer.WebSocketServer;
+import com.kob.backend.pojo.Record;
+
+import java.net.Inet4Address;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class Game {
-    final private Integer rows;
-    final private Integer cols;
-    final private Integer inner_walls_count;
-    final private int[][] g;
-    final private static int[] dx = {-1, 0, 1, 0};
-    final private static int[] dy = {0, 1, 0, -1};
+public class Game extends Thread {
+    private final Integer rows;
+    private final Integer cols;
+    private final Integer inner_walls_count;
+    private final int[][] g;
+    private static final int[] dx = {-1, 0, 1, 0};
+    private static final int[] dy = {0, 1, 0, -1};
 
-    public Game(Integer rows, Integer cols, Integer inner_walls_count) {
+    private final Player playerA, playerB;
+
+    private ReentrantLock lock = new ReentrantLock();
+
+    private String status = "playing";
+    private String loser = ""; // all, a, b,
+
+    private Integer nextStepA = null;
+    private Integer nextStepB = null;
+
+    public Game(Integer rows, Integer cols, Integer inner_walls_count, Integer idA, Integer idB) {
         this.rows = rows;
         this.cols = cols;
         this.inner_walls_count = inner_walls_count;
 
         this.g = new int[rows][cols];
+
+        this.playerA = new Player(idA, rows - 2, 1, new ArrayList<>());
+        this.playerB = new Player(idB, 1, cols - 2, new ArrayList<>());
+    }
+
+    public Player getPlayerA() {
+        lock.lock();
+        try {
+            return playerA;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public Player getPlayerB() {
+        lock.lock();
+        try {
+            return playerB;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void setNextStepA(Integer nextStepA) {
+        this.nextStepA = nextStepA;
+    }
+
+    public void setNextStepB(Integer nextStepB) {
+        this.nextStepB = nextStepB;
     }
 
     public int[][] getG() {
@@ -82,6 +131,165 @@ public class Game {
     public void createMap() {
         for (int i = 0; i < 1000; i++) {
             if (draw()) {
+                break;
+            }
+        }
+    }
+
+    private boolean nextStep() {
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        for (int i = 0; i < 50; i++) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            lock.lock();
+            try {
+                if (nextStepA != null && nextStepB != null) {
+                    playerA.getSteps().add(nextStepA);
+                    playerB.getSteps().add(nextStepB);
+                    return true;
+                }
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        return false;
+    }
+
+    private boolean check_valid(List<Cell> cellsA, List<Cell> cellsB) {
+        int n = cellsA.size();
+        Cell cell = cellsA.get(n - 1);
+        if (g[cell.x][cell.y] == 1) return false;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (cellsA.get(i).x == cell.x && cellsB.get(i).y == cell.y) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            if (cellsB.get(i).x == cell.x && cellsB.get(i).y == cell.y) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void judge() {
+        List<Cell> cellsA = playerA.getCells();
+        List<Cell> cellsB = playerB.getCells();
+
+        boolean validA = check_valid(cellsA, cellsB);
+        boolean validB = check_valid(cellsB, cellsA);
+
+        if (!validA || !validB) {
+            status = "finished";
+
+            if (!validA && !validB) {
+                loser = "all";
+            } else if (!validA) {
+                loser = "A";
+            } else {
+                loser = "B";
+            }
+        }
+    }
+
+    private String getMapString() {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                res.append(g[i][j]);
+            }
+        }
+
+        return res.toString();
+    }
+
+    private void saveToDatabase() {
+        Record record = new Record(
+                null,
+                playerA.getId(),
+                playerA.getSx(),
+                playerA.getSy(),
+                playerB.getId(),
+                playerB.getSx(),
+                playerB.getSy(),
+                playerA.getStepsString(),
+                playerB.getStepsString(),
+                getMapString(),
+                loser,
+                new Date()
+        );
+
+        WebSocketServer.recordMapper.insert(record);
+    }
+
+    private void sendAllMessage(String message) {
+        WebSocketServer.users.get(playerA.getId()).sendMessage(message);
+        WebSocketServer.users.get(playerB.getId()).sendMessage(message);
+    }
+
+    private void sendMove() {
+        lock.lock();
+        try {
+            JSONObject resp = new JSONObject();
+            resp.put("event", "move");
+            resp.put("a_direction", nextStepA);
+            resp.put("b_direction", nextStepB);
+
+            nextStepA = nextStepB = null;
+
+            sendAllMessage(resp.toJSONString());
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private void sendResult() {
+        JSONObject resp = new JSONObject();
+        resp.put("event", "result");
+        resp.put("loser", loser);
+        saveToDatabase();
+        sendAllMessage(resp.toJSONString());
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        for (int i = 0; i < 10000; i++) {
+            if (nextStep()) {
+                judge();
+                if ("playing".equals(status)) {
+                    sendMove();
+                } else {
+                    sendResult();
+                    break;
+                }
+            } else {
+                status = "finished";
+                lock.lock();
+                try {
+                    if (nextStepA == null && nextStepB == null) {
+                        loser = "all";
+                    } else if (nextStepA == null) {
+                        loser = "A";
+                    } else {
+                        loser = "B";
+                    }
+                } finally {
+                    lock.unlock();
+                }
+
+                sendResult();
                 break;
             }
         }
--- web/src/components/GameMap.vue
+++ web/src/components/GameMap.vue
@@ -17,7 +17,10 @@ export default {
         let parent = ref(null);
 
         onMounted(() => {
-            new GameMap(canvas.value.getContext('2d'), parent.value, store);
+          store.commit(
+              "updateGameObject",
+              new GameMap(canvas.value.getContext('2d'), parent.value, store),
+          );
         });
 
         return {
